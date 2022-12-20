1216835b9f7368c29d1047eabc4624e5cdc30247
m-molinari <localhost@localdomain>
2022-12-20 19:45:51 +0100

    improvements

--- consumer.py
+++ consumer.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#! /usr/bin/env python3
 
 import pika
-import argparse
-import sys
-import os
+import argparse, getpass
+import sys, os
+import socket
 import time
 
 # Declare arguments
@@ -13,7 +13,7 @@ parser.add_argument("-q", "--queue", type=str, help="Name of RabbitMQ queue ex: 
 parser.add_argument("-t", "--type", type=str, help="Queue Type: classic/quorum default is classic", default="classic")
 parser.add_argument("-d", "--durable", type=str, help="Queue durable: true/false default is true", default="true")
 parser.add_argument("-u", "--username", type=str, help="username, default is guest", default="guest")
-parser.add_argument("-p", "--password", type=str, help="password, default is guest", default="guest")
+parser.add_argument("-p", "--password", type=str, help="password, default is guest", nargs='?', const='', default="guest")
 parser.add_argument("-v", "--virtualhost", type=str, help="Virtualhost,  default is /", default="/")
 
 # Parse arguments
@@ -26,17 +26,41 @@ Username = args.username
 Password = args.password
 Vhost = args.virtualhost
 
-# credentials
+# Check for remote host connection
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(5)
+result = sock.connect_ex((Rabbit_host,5672))
+if result != 0:
+  print ("Error to connect to remote Host:", Rabbit_host, "on port: 5672 after 5 seconds, please check it")
+  sock.close()
+  sys.exit(2)
+
+# If -p args is empty, ask for password
+if Password == "":
+  Password = getpass.getpass()
+
 credentials = pika.PlainCredentials(Username, Password)
 
-# main function
+# Main function
 def main():
-  connection = pika.BlockingConnection(pika.ConnectionParameters(host=Rabbit_host, port='5672',virtual_host=Vhost, credentials=credentials))
-  channel = connection.channel()
-  channel.queue_declare(queue=Queue, durable=Durable, arguments={'x-queue-type' : Type})
-  print(' [ ] Waiting messages. type CTRL+C for exit')
+  
+  try:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=Rabbit_host, port='5672',virtual_host=Vhost, credentials=credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue=Queue, durable=Durable, arguments={'x-queue-type' : Type})
+    print(' [ ] Waiting messages. type CTRL+C for exit')
+  
+  except pika.exceptions.ProbableAuthenticationError as PAE:
+    print("Connection Error check authentication data, using '-p' arg for password :")
+    print(PAE)
+    sys.exit(2)
 
-  #callback function
+  except UnboundLocalError as UE:
+    print("Connection error!")
+    print(UE)
+    sys.exit(2)
+
+  # Callback function
   def callback(ch, method, properties, body):
       print(" [] Received %r" % body.decode())
       time.sleep(body.count(b'.'))
--- producer.py
+++ producer.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#! /usr/bin/env python3
 
-# import modules
 import pika
 import sys
-import argparse
+import argparse, getpass
+import socket
 
-# declare arguments
+# Declare arguments
 parser = argparse.ArgumentParser(description='RabbitMQ Arguments')
 parser.add_argument("-r", "--rabbithost", type=str, help="RabbitMQ host ex: 127.0.0.1", default="localhost")
 parser.add_argument("-m", "--message", type=str, help="body of message (need quotes if there are spaces), ex : \"hello world\"", default="Body Messagge Example")
@@ -13,11 +13,12 @@ parser.add_argument("-q", "--queue", type=str, help="Name of RabbitMQ queue ex: 
 parser.add_argument("-t", "--type", type=str, help="Queue Type: classic/quorum default is classic", default="classic")
 parser.add_argument("-d", "--durable", type=str, help="Queue durable: true/false default is true", default="true")
 parser.add_argument("-u", "--username", type=str, help="username, default is guest", default="guest")
-parser.add_argument("-p", "--password", type=str, help="password, default is guest", default="guest")
+parser.add_argument("-p", "--password", type=str, help="password, default is guest", nargs='?', const='', default="guest")
 parser.add_argument("-v", "--virtualhost", type=str, help="Virtualhost,  default is /", default="/")
 parser.add_argument("-n", "--numbers", type=int, help="Loop number of messages", default=1)
+parser.add_argument
 
-# parse arguments
+# Parse arguments
 args = parser.parse_args()
 Message = args.message
 Rabbit_host = args.rabbithost
@@ -29,10 +30,21 @@ Password = args.password
 Vhost = args.virtualhost
 Numbers = args.numbers
 
-# credentials
+# Check for remote host connection
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(5)
+result = sock.connect_ex((Rabbit_host,5672))
+if result != 0:
+  print ("Error to connect to remote Host:", Rabbit_host, "on port: 5672 after 5 seconds, please check it")
+  sock.close()
+  sys.exit(2)
+
+# If -p args is empty, ask for password
+if Password == "":
+  Password = getpass.getpass()
+
 credentials = pika.PlainCredentials(Username, Password)
 
-# main function
 def main():
   connection = pika.BlockingConnection(pika.ConnectionParameters(host=Rabbit_host, port='5672',virtual_host=Vhost, credentials=credentials))
   channel = connection.channel()
@@ -51,11 +63,13 @@ if __name__ == '__main__':
   try:
     main()
 
-  except pika.exceptions.AMQPConnectionError:
-      print('Connection Error to RabbitMQ !')
+  except pika.exceptions.AMQPConnectionError as AMQP_C_E:
+      print('Connection Error to RabbitMQ: check authentication data, using "-p" arg for password')
+      print(AMQP_C_E)
 
-  except pika.exceptions.ChannelClosedByBroker:
+  except pika.exceptions.ChannelClosedByBroker as AMQP_CCB:
       print('Check your queue x-queue-type value anche set classic|quorum by -t or --type argument')
+      print(AMQP_CCB)
 
   except :
       print('Something went wrong !')
